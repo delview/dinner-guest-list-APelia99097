953fcc4cfa078d738ed72d0a3c7d8ff0e992630c
APelia99097 <anshveerp097@deltalearns.ca>
2025-02-26 18:34:30 +0000

    Made it s that user can remove someone and then it prints the new group of people

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 
 # Function that greats the user
-# remove function for testing
+
 print("Hello User this is a dinner inviting list ")
 name = input("Whats your name ").capitalize()
 
@@ -8,6 +8,8 @@ name = input("Whats your name ").capitalize()
 guest = []
 # asks the user how many people they would like to add to the list
 num_people = int(input("How many people would you like to invite (number only) "))
+
+
 # A Loop that runs until the num_people in the list are over
 for num in range(num_people):
     # adds the name of the guest to the guest list
@@ -16,6 +18,19 @@ for num in range(num_people):
    print("Name Added!")
 
 
+# A loop that prints a message for each person
 for num in range(num_people):
     print(f"Here is a message for {guest_name}: ")
     print(f"Hello {guest_name} you have been invited to {name}s dinner party" )
+
+while True:
+    kick = input("Would you like to remove someone from the list? [y] or [n]")
+    if kick == 'y':
+        destroy = input("Please Enter the name of the person you want to kick off the list: ")
+        if destroy in guest:
+            guest.remove(destroy)
+            print(f"{destroy} has been removed from the list")
+            print(guest)
+        else:
+            ("Given person is already not in the list")
+
